2ef3a349d1b905eabaac16f3ec5dab78386c8736
Pipirongcha <pipirongcha0x0@gmail.com>
2025-01-04 23:40:42 +0900

    No.2588 Solved

---
+++ No.2588/No.2588/Solution.cs
@@ -0,0 +1,17 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+string tmpNum2 = Console.ReadLine();
+
+int[] num2 = new int[tmpNum2.Length];
+for (int i = 0; i < num2.Length; i++)
+{
+    num2[i] = int.Parse(tmpNum2[i].ToString());
+}
+
+int num3 = num2[2] * num1;
+int num4 = (num2[1] * 10) * num1;
+int num5 = (num2[0] * 100) * num1;
+
+Console.WriteLine(num3);
+Console.WriteLine(num4/10);
+Console.WriteLine(num5/100);
+Console.WriteLine(num3+num4+num5);
