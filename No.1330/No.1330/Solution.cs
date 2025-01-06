8a8aad9ae76584c370fef439b41acf9a652afb3a
Pipirongcha <pipirongcha0x0@gmail.com>
2025-01-06 18:46:31 +0900

    No.1330 Solved

---
+++ No.1330/No.1330/Solution.cs
@@ -0,0 +1,7 @@
+﻿string[] input = Console.ReadLine().Split();
+int a = int.Parse(input[0]);
+int b = int.Parse(input[1]);
+
+if (a > b) Console.WriteLine(">");
+if (a < b) Console.WriteLine("<");
+if (a == b) Console.WriteLine("==");
