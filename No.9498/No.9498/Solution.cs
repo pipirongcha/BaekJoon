904e5ad0ffb934c586f5a422ff89accaeb748301
Pipirongcha <pipirongcha0x0@gmail.com>
2025-01-07 18:14:12 +0900

    No.9498 Solved

---
+++ No.9498/No.9498/Solution.cs
@@ -0,0 +1,7 @@
+﻿string input = Console.ReadLine();
+int score = int.Parse(input);
+if (90 <= score && score <= 100) Console.WriteLine("A");
+else if (80 <= score && score <= 89) Console.WriteLine("B");
+else if (70 <= score && score <= 79) Console.WriteLine("C");
+else if (60 <= score && score <= 69) Console.WriteLine("D");
+else Console.WriteLine("F"); 
