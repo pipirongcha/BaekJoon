065c2200dec563f8af14341c1a458b020173335d
Pipirongcha <pipirongcha0x0@gmail.com>
2025-01-08 21:39:15 +0900

    No.2884 Solved

---
+++ No.2884/No.2884/Solution.cs
@@ -0,0 +1,18 @@
+﻿string[] input = Console.ReadLine().Split();
+
+int H = int.Parse(input[0]);
+int M = int.Parse(input[1]);
+
+M -= 45;
+if (M < 0)
+{
+    M += 60;
+    H --;
+}
+if (H < 0)
+{
+    H = 23;
+}
+string hour = H.ToString();
+string minute = M.ToString();
+Console.WriteLine(hour+" "+minute);
