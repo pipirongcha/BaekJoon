911e37c98473d15439ae8ba4de39579df783d52c
Pipirongcha <pipirongcha0x0@gmail.com>
2025-01-08 21:12:50 +0900

    No.14681 Solved

---
+++ No.14681/No.14681/Solution.cs
@@ -0,0 +1,7 @@
+﻿int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+
+if (x > 0 && y > 0) Console.WriteLine(1);
+if (x < 0 && y > 0) Console.WriteLine(2);
+if (x < 0 && y < 0) Console.WriteLine(3);
+if (x > 0 && y < 0) Console.WriteLine(4);
