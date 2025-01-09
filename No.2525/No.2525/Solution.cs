3cd1735defd568cae02cca8f9ed4aebc3db3e5bf
Pipirongcha <pipirongcha0x0@gmail.com>
2025-01-09 20:44:13 +0900

    No.2525 Solved

---
+++ No.2525/No.2525/Solution.cs
@@ -0,0 +1,19 @@
+﻿string[] input1 = Console.ReadLine().Split();
+int input2 = int.Parse(Console.ReadLine());
+
+int h = int.Parse(input1[0]);
+int m = int.Parse(input1[1]);
+
+m = m + input2;
+
+h += m / 60;
+m = m % 60;
+
+if (h >= 24)
+{
+    h -= 24;
+}
+string hour = h.ToString();
+string min = m.ToString();
+Console.WriteLine(hour + " "+min);
+
