87fac79a0df9ce115bd800658020f1defd7a7b25
Pipirongcha <pipirongcha0x0@gmail.com>
2025-01-10 22:10:45 +0900

    No.2480 Solved

---
+++ No.2480/No.2480/Solution.cs
@@ -0,0 +1,44 @@
+﻿
+
+string[] inputs = Console.ReadLine().Split();
+
+List<int> nums = new List<int>();
+List<int> set = new List<int>();
+int numsVals = 0;
+int setVals = 0;
+
+for (int i = 0; i < inputs.Length; i++)
+{
+    nums.Add(int.Parse(inputs[i]));
+    numsVals += nums[i];
+}
+
+for (int i = 0; i < nums.Count; i++)
+{
+
+    set.Add(nums[i]);
+}
+
+
+set = set.Distinct().ToList();
+
+for (int i = 0; i < set.Count; i++)
+{
+    setVals += set[i];  
+}
+
+if (set.Count == 3)
+{
+    Console.WriteLine(nums.Max() * 100);
+}
+
+if (set.Count == 2)
+{
+    Console.WriteLine(1000 + (numsVals - setVals) * 100);
+}
+
+if (set.Count == 1)
+{
+    Console.WriteLine(10000 + nums[0] * 1000);
+}
+
