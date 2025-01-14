f153b300b846aefaabbbe95c678d84b647fafea3
Pipirongcha <pipirongcha0x0@gmail.com>
2025-01-14 19:08:44 +0900

    No.25304 Solved

---
+++ No.25304/No.25304/Solution.cs
@@ -0,0 +1,17 @@
+﻿int totalCost = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+int result = 0;
+
+for(int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine().Split();
+    result += int.Parse(input[0]) * int.Parse(input[1]);
+}
+if (result == totalCost)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
