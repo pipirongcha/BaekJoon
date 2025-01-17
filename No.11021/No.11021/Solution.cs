08cae8ffe21865bcddbc93ec33641458d4ef3f72
Pipirongcha <pipirongcha0x0@gmail.com>
2025-01-18 05:03:11 +0900

    No.11021 Solved

---
+++ No.11021/No.11021/Solution.cs
@@ -0,0 +1,12 @@
+﻿int t = int.Parse(Console.ReadLine());
+
+
+for (int i = 1; i <= t; i++)
+{
+    string[] input = Console.ReadLine().Split();
+    int a = int.Parse(input[0]);
+    int b = int.Parse(input[1]);
+    int c = a + b;
+    Console.WriteLine("Case #"+i+": " + c);
+
+}
