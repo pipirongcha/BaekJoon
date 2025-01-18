14f1b5c658ff57b054856890abf1ee6038288564
Pipirongcha <pipirongcha0x0@gmail.com>
2025-01-19 04:53:14 +0900

    No.2438 Solved

---
+++ No.2438/No.2438/Solution.cs
@@ -0,0 +1,9 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine());
+StringBuilder star = new StringBuilder();
+for (int i = 1; i <= n; i++)
+{
+    star.Append("*");
+    Console.WriteLine(star);
+}
