0fa92f25a8ab580a72d48a5b3978766177e0fcec
Pipirongcha <pipirongcha0x0@gmail.com>
2025-01-20 19:03:17 +0900

    No.2439 Solved

---
+++ No.2439/No.2439/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine());
+
+StringBuilder result = new StringBuilder();
+
+for (int i = 0; i < n; i++)
+{
+    result.Append(" ");
+}
+
+for (int i = 1; i <= n; i++)
+{
+    result[result.Length -i] = '*';
+    Console.WriteLine(result);
+}
+
