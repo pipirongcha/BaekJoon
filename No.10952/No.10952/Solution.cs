dade4bd38b63c6147fc6ce7b80b68c153ec58236
Pipirongcha <pipirongcha0x0@gmail.com>
2025-01-21 20:14:39 +0900

    No.10952 Solved

---
+++ No.10952/No.10952/Solution.cs
@@ -0,0 +1,12 @@
+﻿string[] input = Console.ReadLine().Split();
+
+int a = int.Parse(input[0]);
+int b = int.Parse(input[1]);
+
+while (a != 0 && b != 0)
+{
+    Console.WriteLine($"{a + b}");
+    input = Console.ReadLine().Split(); 
+    a = int.Parse(input[0]);
+    b = int.Parse(input[1]);
+}
