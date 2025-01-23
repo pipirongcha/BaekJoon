5dad72b0d0211d4a22f7d11529a8348571863ff3
Pipirongcha <pipirongcha0x0@gmail.com>
2025-01-23 18:16:09 +0900

    No.10807 Solved

---
+++ No.10807/No.10807/Solution.cs
@@ -0,0 +1,17 @@
+﻿int n = int.Parse(Console.ReadLine());
+string[] inputs = Console.ReadLine().Split();
+int[] arr = new int[n];
+int v =  int.Parse(Console.ReadLine());
+
+int result = 0; 
+
+for(int i  = 0; i < n; i++)
+{
+    arr[i] = int.Parse(inputs[i]);
+    if (arr[i] == v)
+    {
+        result++;
+    }
+}
+Console.WriteLine(result);
+
