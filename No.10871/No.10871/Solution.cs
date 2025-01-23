c641543865930890b5f50e41eb18781fe89d06ad
Pipirongcha <pipirongcha0x0@gmail.com>
2025-01-24 00:40:12 +0900

    No.10871 Solved

---
+++ No.10871/No.10871/Solution.cs
@@ -0,0 +1,44 @@
+﻿string[] inputs = Console.ReadLine().Split();
+int n = int.Parse(inputs[0]);
+int x = int.Parse(inputs[1]);
+
+string[] arrNums = Console.ReadLine().Split();
+
+string result = "";
+
+for (int i = 0; i < n; i++)
+{
+    if (int.Parse(arrNums[i]) < x)
+    {
+        result += arrNums[i] + " ";
+    }
+}
+
+Console.WriteLine(result);
+
+
+
+//string[] inputs = Console.ReadLine().Split();
+//int n = int.Parse(inputs[0]);
+//int x = int.Parse(inputs[1]);
+
+//string[] arrNums = Console.ReadLine().Split();
+
+//int[] arr = new int[n];
+
+//for (int i = 0; i < arrNums.Length; i++)
+//{
+//    arr[i] = int.Parse(arrNums[i]);
+//}
+
+//string result = "";
+
+//for (int i = 0; i < n; i++)
+//{
+//    if (arr[i] < x)
+//    {
+//        result += arr[i].ToString() + " ";
+//    }
+//}
+
+//Console.WriteLine(result);
