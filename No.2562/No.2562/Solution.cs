d9b658087d008bab2a6b387f8511088160bbd810
Pipirongcha <pipirongcha0x0@gmail.com>
2025-01-26 04:00:41 +0900

    No.2562 Solved

---
+++ No.2562/No.2562/Solution.cs
@@ -0,0 +1,16 @@
+﻿int[] arr = new int[9];
+int max = 0;
+int idx = 0;
+
+for (int i = 0; i < 9; i++)
+{
+    arr[i] = int.Parse(Console.ReadLine());
+    if (arr[i] >= max)
+    {
+        max = arr[i];
+        idx = i + 1;
+    }
+}
+
+Console.WriteLine($"{max}");
+Console.WriteLine($"{idx}");
