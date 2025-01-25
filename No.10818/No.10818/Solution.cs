288dfe78fe0e7d4a06a73298f3c512fe4e277a4b
Pipirongcha <pipirongcha0x0@gmail.com>
2025-01-25 22:35:15 +0900

    No.10818 Solved

---
+++ No.10818/No.10818/Solution.cs
@@ -0,0 +1,14 @@
+﻿int n = int.Parse(Console.ReadLine());
+string[] nums = Console.ReadLine().Split();
+
+int min = 1000000;
+int max = -1000000;
+
+for  (int i = 0; i < n; i++)
+{
+    int target = int.Parse(nums[i]);
+    if (target < min) min = target;
+    if (target > max) max = target;
+}
+
+Console.WriteLine($"{min} {max}");
