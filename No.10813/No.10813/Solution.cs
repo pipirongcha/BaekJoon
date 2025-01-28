ec37cf987eddbc4272f65770b6f7be72d0a07347
Pipirongcha <pipirongcha0x0@gmail.com>
2025-01-28 21:14:35 +0900

    No.10813 Solved

---
+++ No.10813/No.10813/Solution.cs
@@ -0,0 +1,26 @@
+﻿string[] inputs = Console.ReadLine().Split();
+int n = int.Parse(inputs[0]);
+int m = int.Parse(inputs[1]);
+
+int i;
+int j;
+string[] inputs2;
+int[] res = new int[n];
+
+for (int a = 0; a < n; a++)
+{
+    res[a] = a+1;
+}
+
+for (int a = 0; a < m; a++)
+{
+    inputs2 = Console.ReadLine().Split();
+    i = int.Parse(inputs2[0])-1;
+    j = int.Parse(inputs2[1])-1;
+    int tmp = res[i];
+    res[i] = res[j];
+    res[j] = tmp;
+
+}
+
+Console.WriteLine(string.Join(" ",res));
