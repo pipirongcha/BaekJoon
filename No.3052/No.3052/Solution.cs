2743a0d7f4f8db1f8ebc0727ca6dc5348491da57
Pipirongcha <pipirongcha0x0@gmail.com>
2025-01-30 00:16:18 +0900

    No.3052 Solved

---
+++ No.3052/No.3052/Solution.cs
@@ -0,0 +1,13 @@
+﻿int res;
+int n;
+List<int> list = new List<int>();
+
+for (int i = 0; i < 10; i++)
+{
+    n = int.Parse(Console.ReadLine());
+    list.Add(n%42);
+}
+
+list = list.Distinct().ToList();
+
+Console.WriteLine(list.Count);
