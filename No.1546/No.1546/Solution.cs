2e33dfe3dfc7f50524b5265e9050174d8e0eaa0d
Pipirongcha <pipirongcha0x0@gmail.com>
2025-02-01 00:46:01 +0900

    No.1546 Solved

---
+++ No.1546/No.1546/Solution.cs
@@ -0,0 +1,23 @@
+﻿int n = int.Parse(Console.ReadLine());
+string[] tmp = Console.ReadLine().Split();
+float[] list = new float[n];
+float max = 0;
+float res = 0;
+
+for (int i = 0; i < tmp.Length; i++)
+{
+    list[i] = int.Parse(tmp[i]);
+    if (list[i] > max)
+    {
+        max = list[i];
+    }
+
+}
+
+for (int i = 0; i < n; i++)
+{
+    list[i] = list[i] / max * 100;
+    res += list[i];
+}
+
+Console.WriteLine(res/n);
