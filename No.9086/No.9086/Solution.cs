7b12dbe4837b119a2970bb622659353a66c3d4a8
Pipirongcha <pipirongcha0x0@gmail.com>
2025-02-02 00:17:40 +0900

    No,9086 Solved

---
+++ No.9086/No.9086/Solution.cs
@@ -0,0 +1,9 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string str = Console.ReadLine();
+    Console.WriteLine($"{str[0]}{str[str.Length-1]}");
+}
