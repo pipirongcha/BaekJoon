43588171d709444d01895db5e4ac8f90ce5c9a63
Pipirongcha <pipirongcha0x0@gmail.com>
2025-02-03 09:31:53 +0900

    No.11720 Solved

---
+++ No.11720/No.11720/Solution.cs
@@ -0,0 +1,9 @@
+﻿int n = int.Parse(Console.ReadLine());
+string str = Console.ReadLine();
+int res = 0;
+for (int i = 0; i < n; i++)
+{
+    int num = int.Parse($"{str[i]}");
+    res += num;
+}
+Console.WriteLine(res);
