e69b20e00c62427497efe874ac4585da57099376
Pipirongcha <pipirongcha0x0@gmail.com>
2025-02-03 09:39:09 +0900

    No.11720( Add better solving)

--- No.11720/No.11720/Solution.cs
+++ No.11720/No.11720/Solution.cs
@@ -7,3 +7,18 @@ for (int i = 0; i < n; i++)
     res += num;
 }
 Console.WriteLine(res);
+
+
+//정석 풀이
+
+/*
+int n = int.Parse(Console.ReadLine());
+string str = Console.ReadLine();
+int res = 0;
+for (int i = 0; i < n; i++)
+{
+    res += str[i] - '0'; 
+}
+
+Console.WriteLine(res);
+*/
