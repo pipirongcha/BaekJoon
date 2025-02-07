ffc8927e475f7246353a123617c85a2e3c54111c
Pipirongcha <pipirongcha0x0@gmail.com>
2025-02-07 09:08:34 +0900

    No.25083 Solved

---
+++ No.25083/No.25083/Solution.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("         ,r'\"7");
+Console.WriteLine("r`-_   ,'  ,/");
+Console.WriteLine(" \\. \". L_r'");
+Console.WriteLine("   `~\\/");
+Console.WriteLine("      |");
+Console.WriteLine("      |");
