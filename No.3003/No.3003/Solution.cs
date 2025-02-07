ee6067ad437fe0d35483ae653c437d3829102b41
Pipirongcha <pipirongcha0x0@gmail.com>
2025-02-07 09:24:47 +0900

    No.3003 Solved

---
+++ No.3003/No.3003/Solution.cs
@@ -0,0 +1,8 @@
+﻿int[] res = { 1, 1, 2, 2, 2, 8 };
+string[] inputs = Console.ReadLine().Split();
+
+for (int i = 0; i < inputs.Length; i++)
+{
+    res[i] -= int.Parse(inputs[i]);
+}
+Console.WriteLine(string.Join(" ",res));
