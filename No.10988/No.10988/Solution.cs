04cb8645580692817ce6afe78ab4c15e4fd51413
Pipirongcha <pipirongcha0x0@gmail.com>
2025-02-09 19:13:32 +0900

    No.10988 Solved

---
+++ No.10988/No.10988/Solution.cs
@@ -0,0 +1,51 @@
+﻿char[] input = Console.ReadLine().ToCharArray();
+int front = 0;
+int terminal = input.Length - 1;
+int res = 0;
+
+
+if(front == terminal)
+{
+    res = 1;
+    Console.WriteLine(res);
+}
+
+else if (input.Length % 2 != 0)
+{
+    while (terminal != front)
+    {
+        if (input[front] == input[terminal])
+        {
+            front++;
+            terminal--;
+            res = 1;
+        }
+        else
+        {
+            res = 0;
+            break;
+        }
+
+    }
+    Console.WriteLine(res);
+}
+
+else
+{
+    while (terminal > front)
+    {
+        if (input[front] == input[terminal])
+        {
+            front++;
+            terminal--;
+            res = 1;
+        }
+        else
+        {
+            res = 0;
+            break;
+        }
+
+    }
+    Console.WriteLine(res);
+}
