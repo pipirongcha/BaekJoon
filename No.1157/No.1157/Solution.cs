6f253a5ced1294619e0052364d233e3ac2889c78
Pipirongcha <pipirongcha0x0@gmail.com>
2025-02-10 11:27:07 +0900

    No.1157 Solved

---
+++ No.1157/No.1157/Solution.cs
@@ -0,0 +1,34 @@
+﻿string input = Console.ReadLine();
+input = input.ToUpper();
+int[] arr = new int[26];
+
+for (int i = 0; i < input.Length; i++)
+{
+    arr[input[i] - 65] ++;
+}
+
+int max = arr.Max();
+int count = 0;
+
+for (int i = 0;i < arr.Length;i++)
+{
+    if (arr[i] == max)
+    {
+        count++;
+    }
+    if (count > 1)
+    {
+        break;
+    }
+
+}
+
+if (count >1)
+{
+    Console.WriteLine("?");
+}
+else
+{
+    int idx = Array.IndexOf(arr, max)+65;
+    Console.WriteLine((char)idx);
+}
