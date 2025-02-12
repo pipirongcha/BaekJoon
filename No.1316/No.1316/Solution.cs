80ed1fd6ace44cddd2b76bab20ea43a12ff8d137
Pipirongcha <pipirongcha0x0@gmail.com>
2025-02-12 15:59:10 +0900

    No.1316 Solved

---
+++ No.1316/No.1316/Solution.cs
@@ -0,0 +1,40 @@
+﻿int n = int.Parse(Console.ReadLine());
+string target;
+int res = 0;
+List<char> usedChar = new List<char>(); 
+
+for (int i = 0; i < n; i++)
+{
+    target = Console.ReadLine();
+    if (target.Length == target.Distinct().Count())
+    {
+        res++;
+    }
+    else
+    {
+        for (int j = 0; j < target.Length; j++)
+        {
+            char tmp = target[j];
+            if (j+1 < target.Length && tmp == target[j+1])
+            {
+                continue;
+            }
+            else
+            {
+                usedChar.Add(tmp);
+            }
+        }
+        if (usedChar.Count == usedChar.Distinct().ToList().Count)
+        {
+            res++;
+            usedChar.Clear();
+        }
+        else
+        {
+            usedChar.Clear();
+        }
+    }
+
+}
+
+Console.WriteLine(res);
