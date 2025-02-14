c1c594cf2937b74ab24c2ae0c4fd22105aca0b2a
Pipirongcha <pipirongcha0x0@gmail.com>
2025-02-15 01:35:24 +0900

    No.2566 Solved

---
+++ No.2566/No.2566/Solution.cs
@@ -0,0 +1,22 @@
+﻿int max = 0;
+int maxN = 0;
+int maxM = 0;
+for (int i = 0; i < 9; i++)
+{
+    string[] input = Console.ReadLine().Split();
+    for (int j = 0; j < 9; j++)
+    {
+        int val = int.Parse(input[j]);
+
+        if (max < val)
+        {
+            max = val;
+            maxN = i;
+            maxM = j;
+        }
+            
+    }
+}
+
+Console.WriteLine(max);
+Console.WriteLine($"{maxN+1} {maxM+1}");
