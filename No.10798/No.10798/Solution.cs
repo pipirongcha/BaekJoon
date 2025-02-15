f00184e610ea4892d0058f5ec649f56d3f638b57
Pipirongcha <pipirongcha0x0@gmail.com>
2025-02-16 03:58:59 +0900

    No.10798 Solved

---
+++ No.10798/No.10798/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+char[,] matrix = new char[5,15];
+StringBuilder res = new StringBuilder();
+
+for(int i = 0; i < 5; i++)
+{
+    string input = Console.ReadLine();
+    for(int j = 0; j < input.Length; j++)
+    {
+        matrix[i,j] = input[j];
+    }
+}
+
+for (int i = 0; i < 15; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        if(matrix[j,i] != '\0')
+        {
+            res.Append(matrix[j, i]);
+        }
+    }
+}
+
+Console.WriteLine(res.ToString());    
