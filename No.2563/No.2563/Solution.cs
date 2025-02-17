6473fca379f8f5e19d7682ea6924d6970c6d2356
Pipirongcha <pipirongcha0x0@gmail.com>
2025-02-17 13:41:29 +0900

    No.2563 Solved

---
+++ No.2563/No.2563/Solution.cs
@@ -0,0 +1,23 @@
+﻿int n = int.Parse(Console.ReadLine());
+bool[,] space = new bool[100,100];
+int res = 0;
+for (int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine().Split();
+    int a = int.Parse(input[0]);
+    int b = int.Parse(input[1]);
+   for (int j = a; j < a+10; j++)
+    {
+        for (int k = b; k < b+10; k++)
+        {
+            if (!space[j, k])
+            {
+                space[j, k] = true;
+                res++;
+                
+            }
+        }
+    }
+}
+
+Console.WriteLine(res);
