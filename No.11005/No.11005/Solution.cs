ec5158536fb3b14cc90536a90ef76bb6b0e0b4c3
Pipirongcha <pipirongcha0x0@gmail.com>
2025-02-19 04:42:19 +0900

    No.11005 Solved

---
+++ No.11005/No.11005/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+Dictionary<int, char> dict = new Dictionary<int, char>();
+
+for (int i = 0; i <26; i++)
+{
+    dict.Add(10+i,(char)(65 + i));
+}
+
+string[] input = Console.ReadLine().Split();
+int num = int.Parse(input[0]);
+int numeral = int.Parse(input[1]);
+StringBuilder res = new StringBuilder();
+
+while (num > 0)
+{
+
+    int calculate = num%numeral;
+    if (calculate <= 9)
+    {
+        res.Insert(0, calculate);
+    }
+    else
+    {
+        res.Insert(0, dict[calculate]);
+    }
+    num = num/numeral;
+}
+
+Console.WriteLine(res.ToString());
+
