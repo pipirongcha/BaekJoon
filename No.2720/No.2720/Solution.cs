29a2c39f76f9327739c70ba5f46567f75d31abf7
Pipirongcha <pipirongcha0x0@gmail.com>
2025-02-20 00:58:23 +0900

    No.2720 Solved

---
+++ No.2720/No.2720/Solution.cs
@@ -0,0 +1,35 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int quarter = 0;
+    int dime = 0;
+    int nickel = 0;
+    int penny = 0;
+    int input = int.Parse(Console.ReadLine());
+
+    while (input >= 25)
+    {
+        quarter++;
+        input -= 25;
+    }
+
+    while (input >= 10)
+    {
+        dime++;
+        input -= 10;
+    }
+
+    while (input >= 5)
+    {
+        nickel++;
+        input -= 5;
+    }
+
+    while (input >= 1)
+    {
+        penny++;
+        input -= 1;
+    }
+    Console.WriteLine($"{quarter} {dime} {nickel} {penny}");
+}
