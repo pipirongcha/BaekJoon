64ec8f20cc98c57e1b4b8f02d51305c2b11e59e2
Pipirongcha <pipirongcha0x0@gmail.com>
2025-02-24 04:24:46 +0900

    No.2869 Solved

---
+++ No.2869/No.2869/Solution.cs
@@ -0,0 +1,16 @@
+﻿string[] input = Console.ReadLine().Split();
+int a = int.Parse(input[0]);
+int b = int.Parse(input[1]);
+int v = int.Parse(input[2]);
+
+int res = ((v-a) / (a - b))+1;
+int check = (v-a) % (a - b);
+
+if (check == 0)
+{
+    Console.WriteLine(res);
+}
+else
+{
+    Console.WriteLine(res + 1);
+}    
