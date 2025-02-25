0cc85ddfef9ca1a72548699392a6b140f6af8583
Pipirongcha <pipirongcha0x0@gmail.com>
2025-02-26 04:10:48 +0900

    No.2501 Solved

---
+++ No.2501/No.2501/Solution.cs
@@ -0,0 +1,21 @@
+﻿string[] input = Console.ReadLine().Split();
+int n = int.Parse(input[0]);
+int k = int.Parse(input[1]);
+List<int> list = new List<int>();
+
+for (int i = 0; i < n; i++)
+{
+    if (n%(n-i) == 0)
+    {
+        list.Add(n-i);
+    }
+}
+
+if (list.Count >= k)
+{
+    Console.WriteLine(list[list.Count-k]);
+}
+else
+{
+    Console.WriteLine(0);
+}
