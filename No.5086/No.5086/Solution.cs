c03a483752068b791c0030798cb28c30eb70375b
Pipirongcha <pipirongcha0x0@gmail.com>
2025-02-25 11:18:43 +0900

    No.5086 Solved

---
+++ No.5086/No.5086/Solution.cs
@@ -0,0 +1,22 @@
+﻿string[] input = Console.ReadLine().Split();
+int a = int.Parse(input[0]);
+int b = int.Parse(input[1]);
+
+while (a != b)
+{
+    if (b%a == 0)
+    {
+        Console.WriteLine("factor");
+    }
+    else if(a%b == 0)
+    {
+        Console.WriteLine("multiple");
+    }
+    else
+    {
+        Console.WriteLine("neither");
+    }
+    input = Console.ReadLine().Split();
+    a = int.Parse(input[0]);
+    b = int.Parse(input[1]);
+}
