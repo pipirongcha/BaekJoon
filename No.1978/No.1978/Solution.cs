53077d5f5ef9dd24c67cfb082a885ce0f6857e6d
Pipirongcha <pipirongcha0x0@gmail.com>
2025-02-28 09:53:09 +0900

    No.1978 Solved

---
+++ No.1978/No.1978/Solution.cs
@@ -0,0 +1,34 @@
+﻿int n = int.Parse(Console.ReadLine());
+string[] input = Console.ReadLine().Split();
+int res = 0;
+
+for (int i = 0; i < n; i++)
+{
+    int target = int.Parse(input[i]);
+    int j = 1;
+    while(j <= target)
+    {
+        if(target%j == 0)
+        {
+            if (j == 1 || j == i)
+            {
+                j++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        else
+        {
+            j++;
+        }
+    }
+    if (j==target)
+    {
+        res++;
+    }
+    j = 0;
+}
+
+Console.WriteLine(res);
