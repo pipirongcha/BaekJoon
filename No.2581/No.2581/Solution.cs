b56549d20bf259119b1420a5f4fad1a3425b0b04
Pipirongcha <pipirongcha0x0@gmail.com>
2025-03-01 04:20:14 +0900

    No.2581 Solved

---
+++ No.2581/No.2581/Solution.cs
@@ -0,0 +1,42 @@
+﻿int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+
+int sum = 0;
+int min = 10000;
+
+for (int i = m; i <= n; i++)
+{
+    int j = 2;
+    while (j < i)
+    {
+        if (i % j == 0)
+        {
+            if (j != 1 && j != i)
+            {
+                break;
+            }
+            j++;
+        }
+        else 
+        {
+            j++; 
+        }
+
+    }
+    if (j == i)
+    {
+        sum += j;
+        if (j < min)
+        {
+            min = j;
+        }
+    }
+    j = 2;
+}
+
+if (sum != 0)
+{
+    Console.WriteLine(sum);
+    Console.WriteLine(min);
+}
+else { Console.WriteLine(-1); }
