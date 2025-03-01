103f2bc7fe3485372a47098ff89b9a7915513b52
Pipirongcha <pipirongcha0x0@gmail.com>
2025-03-02 05:08:00 +0900

    No.11653 Solved

---
+++ No.11653/No.11653/Solution.cs
@@ -0,0 +1,17 @@
+﻿int input = int.Parse(Console.ReadLine());
+int tmp = 2;
+
+while (input != 1)
+{
+    if (input % tmp == 0)
+    {
+        Console.WriteLine(tmp);
+        input /= tmp;
+    }
+    else
+    {
+        tmp++;
+    }
+}
+
+//더 큰 합성수는 이미 소수를 체크했기 때문에 굳이 소수인지 확인할 필요가 없다...
