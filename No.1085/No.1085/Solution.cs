8cb303161d7c7a378eede13ab4f588e03943e0a8
Pipirongcha <pipirongcha0x0@gmail.com>
2025-03-03 14:41:15 +0900

    No.1085 Solved

---
+++ No.1085/No.1085/Solution.cs
@@ -0,0 +1,10 @@
+﻿string[] input = Console.ReadLine().Split();
+float x = float.Parse(input[0]);
+float y = float.Parse(input[1]);
+
+float n = float.Parse(input[2])-x;
+float m = float.Parse(input[3])-y;
+
+float a = MathF.Min(x, y);
+float b = MathF.Min(n, m);
+Console.WriteLine(MathF.Min(a, b));
