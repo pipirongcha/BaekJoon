32819467907addf62bd70c2ec958dd02cd5c53b1
Pipirongcha <pipirongcha0x0@gmail.com>
2025-03-07 08:13:47 +0900

    No.10101 Solved

---
+++ No.10101/No.10101/Solution.cs
@@ -0,0 +1,23 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+if (a+b+c == 180)
+{
+    if (a == b && b == c)
+    {
+        Console.WriteLine("Equilateral");
+    }
+    else if (a == b || b == c || a == c)
+    {
+        Console.WriteLine("Isosceles");
+    }
+    else
+    {
+        Console.WriteLine("Scalene");
+    }
+}
+else
+{
+    Console.WriteLine("Error");
+}
