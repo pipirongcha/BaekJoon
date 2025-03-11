4ac094599ba8ab3b9b2f662720b18830c30678fd
Pipirongcha <pipirongcha0x0@gmail.com>
2025-03-11 09:18:00 +0900

    No.24265 Solved

---
+++ No.24265/No.24265/Solution.cs
@@ -0,0 +1,13 @@
+﻿//long n = long.Parse(Console.ReadLine());
+//long res = 0;
+//for (long i = 1; i < n; i++)
+//{
+//    res += n - i;
+//}
+
+//Console.WriteLine(res);
+//Console.WriteLine(2);
+
+long n = long.Parse(Console.ReadLine());
+Console.WriteLine((n - 1) * n / 2);
+Console.WriteLine(2);
