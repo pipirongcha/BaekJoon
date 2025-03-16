fc449de412bc62781aeff0b2023b5a55b29aa941
Pipirongcha <pipirongcha0x0@gmail.com>
2025-03-16 18:47:44 +0900

    No.2231 Solved

---
+++ No.2231/No.2231/Solution.cs
@@ -0,0 +1,26 @@
+﻿string input = Console.ReadLine();
+int n = int.Parse(input);
+int tmp = 0;
+bool done = false;
+
+for (int i = 1; i < n; i++)
+{
+    string a = i.ToString();
+    for (int j = 0; j < a.Length; j++)
+    {
+        tmp += a[j] - 48;
+    }
+    int res = i + tmp; 
+    if (res == n)
+    {
+        Console.WriteLine(i);
+        done = true;
+        break;
+    }
+    tmp = 0;
+}
+
+if(!done)
+{
+    Console.WriteLine(0);
+}
