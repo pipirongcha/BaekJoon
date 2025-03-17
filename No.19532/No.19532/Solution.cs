b4b6f7c4287a801f331ff2d4288ba60aa51f62f8
Pipirongcha <pipirongcha0x0@gmail.com>
2025-03-17 18:05:03 +0900

    No.15932 Solved

---
+++ No.19532/No.19532/Solution.cs
@@ -0,0 +1,24 @@
+﻿string[] input = Console.ReadLine().Split();
+
+int a = int.Parse(input[0]);
+int b = int.Parse(input[1]);
+int c = int.Parse(input[2]);
+int d = int.Parse(input[3]);
+int e = int.Parse(input[4]);
+int f = int.Parse(input[5]);
+
+
+for(int x = -999; x <= 999; x++)
+{
+    for(int y = -999; y <= 999; y++)
+    {
+        if ((a * x) + (b * y) == c)
+        {
+            if ((d * x) + (e * y) == f)
+            {
+                Console.WriteLine(x);
+                Console.WriteLine(y);
+            }
+        }
+    }
+}
