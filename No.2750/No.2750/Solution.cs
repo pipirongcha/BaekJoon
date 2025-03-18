21981c03e3c282e0a1ce8f7173a2550b97b35cbb
Pipirongcha <pipirongcha0x0@gmail.com>
2025-03-18 23:40:44 +0900

    No.2750 Solved

---
+++ No.2750/No.2750/Solution.cs
@@ -0,0 +1,26 @@
+﻿int n = int.Parse(Console.ReadLine());
+List<int> list = new List<int>();
+
+for (int i = 0; i < n; i++)
+{
+    int input = int.Parse(Console.ReadLine());
+    list.Add(input);
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < list.Count; j++)
+    {
+        if (list[i] < list[j])
+        {
+            int tmp = list[j];
+            list[j] = list[i];
+            list[i] = tmp;
+        }
+    }
+}
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine(list[i]);
+}
