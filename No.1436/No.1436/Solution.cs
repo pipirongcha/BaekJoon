74ad26b6390e72e4ef37eeb776e6f1046bf57879
Pipirongcha <pipirongcha0x0@gmail.com>
2025-03-20 21:26:45 +0900

    No.1436 Solved

---
+++ No.1436/No.1436/Solution.cs
@@ -0,0 +1,29 @@
+﻿int n = int.Parse(Console.ReadLine());
+int m = 0;
+long res = 666;
+bool endNum = false;
+
+while (m != n)
+{
+    string tmp = res.ToString();
+    for (int i = 0; i <tmp.Length-2; i++)
+    {
+        if (tmp[i] == '6' && tmp[i+1] == '6' && tmp[i+2] == '6')
+        {
+            endNum = true;
+        }
+    }
+    if (endNum)
+    {
+        m++;
+    }
+    if(m==n)
+    {
+        break;
+    }
+    res++;
+    endNum = false;
+
+}
+
+Console.WriteLine(res);
