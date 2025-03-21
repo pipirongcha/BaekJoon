695754bfa519f011285c6e75ee3fb12f80f1bfa2
Pipirongcha <pipirongcha0x0@gmail.com>
2025-03-21 16:17:09 +0900

    No.2839 Solved

---
+++ No.2839/No.2839/Solution.cs
@@ -0,0 +1,14 @@
+﻿int n = int.Parse(Console.ReadLine());
+int res = -1;
+
+for (int i = n / 5; i >= 0; i--)
+{
+    int tmp = n -(5*i);
+    if (tmp % 3 == 0)
+    {
+        int a = i + (tmp / 3);
+        res = a;
+        break;
+    }
+}
+Console.WriteLine(res);
