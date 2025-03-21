0a6883afe7534476760c18c737c81f6bde1cd7f6
Pipirongcha <pipirongcha0x0@gmail.com>
2025-03-22 01:12:44 +0900

    No.2587 Solved

---
+++ No.2587/No.2587/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+int avg = 0;
+List<int> list = new List<int>();
+
+for(int i = 0; i < 5; i++)
+{
+    int input = int.Parse(Console.ReadLine());
+    list.Add(input); 
+    
+    avg += input;
+}
+for (int i = 0;i < list.Count;i++)
+{
+    for (int j = 0;j < list.Count;j++)
+    {
+        if (list[i] > list[j])
+        {
+            int tmp = list[i];
+            list[i] = list[j];
+            list[j] = tmp;
+        }
+    }
+}
+Console.WriteLine(avg/5);
+Console.WriteLine(list[2]);
