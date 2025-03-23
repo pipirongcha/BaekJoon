19aef2a522f09e76f4995deaebad7281af832f36
Pipirongcha <pipirongcha0x0@gmail.com>
2025-03-23 13:30:25 +0900

    No.25305 Solved

---
+++ No.25305/No.25305/Solution.cs
@@ -0,0 +1,20 @@
+﻿int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int n = input[0];
+int k = input[1];
+
+int[] nums = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+for (int i = 0; i < n - 1; i++)
+{
+    for (int j = 0; j < n - 1 - i; j++)
+    {
+        if (nums[j] < nums[j+1])
+        {
+            int tmp = nums[j];
+            nums[j] = nums[j+1];
+            nums[j+1] = tmp;
+        }
+    }
+}
+
+Console.WriteLine(nums[k - 1]);
