acfd616d3df0231e3d911f36356b2ad3481c8314
Pipirongcha <pipirongcha0x0@gmail.com>
2025-03-23 14:28:43 +0900

    No.1547 Solved

---
+++ No.1547/No.1547/Solution.cs
@@ -0,0 +1,37 @@
+﻿int m = int.Parse(Console.ReadLine());
+List<bool> list = new List<bool>();
+
+for (int i = 0; i < 3; i++)
+{
+    if (i == 0)
+    {
+        list.Add(true);
+    }
+    else
+    {
+        list.Add(false);
+    }
+}
+
+for (int i = 0; i < m; i++)
+{
+    int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    int a = input[0] - 1;
+    int b = input[1] - 1;
+
+    bool tmp = list[a];
+    list[a] = list[b];
+    list[b] = tmp;
+
+
+
+}
+
+for (int i = 0;i < 3; i++)
+{
+    if (list[i])
+    {
+        Console.WriteLine(i+1);
+        break;
+    }
+}
