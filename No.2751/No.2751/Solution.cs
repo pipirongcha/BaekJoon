dc7c1f8ea6b437432fe0820e323a7de4036ebc97
Pipirongcha <pipirongcha0x0@gmail.com>
2025-03-24 20:21:03 +0900

    No.2751 Solved

---
+++ No.2751/No.2751/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine());
+List<int> list = new List<int>();
+List<int> res = new List<int>();
+StringBuilder sb = new StringBuilder();
+
+for (int i = 0; i < n; i++)
+{ 
+    list.Add(int.Parse(Console.ReadLine()));
+}
+
+res = list.Distinct().ToList();
+res.Sort();
+
+for (int i = 0; i < res.Count; i++)
+{
+    sb.AppendLine(res[i].ToString());
+}
+
+Console.WriteLine(sb.ToString()); 
+
