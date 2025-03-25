17b97d23cf38108a3f5b55f105a108c2cfee7621
Pipirongcha <pipirongcha0x0@gmail.com>
2025-03-25 20:42:11 +0900

    No.10989 Solved

---
+++ No.10989/No.10989/Solution.cs
@@ -0,0 +1,20 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+int[] nums = new int[10001];
+
+int n = int.Parse(sr.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    nums[int.Parse(sr.ReadLine())]++;
+}
+
+for (int i = 0; i < nums.Length; i++)
+{
+    while (nums[i] != 0)
+    {
+        sw.WriteLine(i);
+        nums[i]--;
+    }
+}
+sw.Flush();
