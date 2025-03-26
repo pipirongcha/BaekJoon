bc49599f5776dca113e9b3fc72ab63967848222d
Pipirongcha <pipirongcha0x0@gmail.com>
2025-03-26 18:37:50 +0900

    No.1427 Solved

---
+++ No.1427/No.1427/Solution.cs
@@ -0,0 +1,18 @@
+﻿StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+string input = Console.ReadLine();
+int n = input.Length;
+List<int> nums = new List<int>();
+
+for (int i = 0; i < n; i++)
+{
+    nums.Add(input[i]-48);
+}
+
+nums.Sort();
+
+for (int i = n-1;i >= 0; i--)
+{
+    sw.Write(nums[i]);
+}
+
+sw.Flush();
