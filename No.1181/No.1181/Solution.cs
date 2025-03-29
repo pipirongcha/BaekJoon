cf80eb801145fc32acab8b29232cae00e09ab1df
Pipirongcha <pipirongcha0x0@gmail.com>
2025-03-29 12:24:14 +0900

    No.1181 Solved

---
+++ No.1181/No.1181/Solution.cs
@@ -0,0 +1,41 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+int n = int.Parse(sr.ReadLine());
+string[] input = new string[n];
+for (int i = 0; i < n; i++)
+{
+    input[i] = sr.ReadLine();
+}
+
+input = input.Distinct().ToArray();
+
+Array.Sort(input,(a,b) =>
+{
+    if(a.Length != b.Length) 
+        return a.Length.CompareTo(b.Length);
+    else
+    {
+        for(int i = 0;i < a.Length;i++)
+        {
+            if(a[i] < b[i]) 
+                return -1;
+            else if (a[i] == b[i])
+            {
+                continue;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        return 1;
+    }
+});
+
+for (int i = 0; i <input.Length; i++)
+{
+    sw.WriteLine(input[i]);
+}
+
+sw.Flush();
