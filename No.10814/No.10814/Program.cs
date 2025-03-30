17c4f6598db3b30e5a00221e26c4cfb50ce60b64
Pipirongcha <pipirongcha0x0@gmail.com>
2025-03-30 21:52:56 +0900

    No.10814 Solved

---
+++ No.10814/No.10814/Program.cs
@@ -0,0 +1,33 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+int n = int.Parse(sr.ReadLine());
+
+string[][] list = new string[n][];
+for (int i = 0; i < n; i++)
+{
+    string[] input = sr.ReadLine().Split();
+    input = input.Append(i.ToString()).ToArray();
+    list[i] = input;
+
+}
+
+Array.Sort(list, (a, b) =>
+{
+    int n = int.Parse(a[2]);
+    int m = int.Parse(b[2]);
+    int l = int.Parse(a[0]);
+    int k = int.Parse(b[0]);
+    if (l == k)
+    {
+        return n.CompareTo(m);
+    }
+    return l.CompareTo(k);
+});
+
+for (int i = 0;i < n; i++)
+{
+    sw.WriteLine($"{list[i][0]} {list[i][1]}");
+}
+
+sw.Flush();
