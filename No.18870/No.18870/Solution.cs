57260d81b687298841de19ebfe46a63d1b0ea209
Pipirongcha <pipirongcha0x0@gmail.com>
2025-03-31 10:55:06 +0900

    No.18870 Solved

---
+++ No.18870/No.18870/Solution.cs
@@ -0,0 +1,28 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+Dictionary<int, int> dict = new Dictionary<int, int>();
+
+int n = int.Parse(sr.ReadLine());
+int[] nums = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+int[] sortedNums = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    sortedNums[i] = nums[i];
+}
+
+Array.Sort(sortedNums);
+sortedNums = sortedNums.Distinct().ToArray();
+
+for (int i = 0; i < sortedNums.Length; i++)
+{
+    dict[sortedNums[i]] = i;
+}
+
+for (int i = 0; i < n; i++)
+{
+    nums[i] = dict[nums[i]];
+    sw.Write(nums[i] + " ");
+}
+
+sw.Flush();
