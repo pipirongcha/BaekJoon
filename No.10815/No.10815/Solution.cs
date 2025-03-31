823abc39887f60da16c4b4b7793a490bfd978fce
Pipirongcha <pipirongcha0x0@gmail.com>
2025-04-01 02:02:11 +0900

    No.10815 Solved

---
+++ No.10815/No.10815/Solution.cs
@@ -0,0 +1,26 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+int n = int.Parse(sr.ReadLine());
+int[] numsN = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+int m = int.Parse(sr.ReadLine());
+int[] numsM = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+
+Dictionary<int, int> dict = new Dictionary<int, int>();
+
+for(int i = 0; i < m; i++)
+{
+    dict[numsM[i]] = 0;
+}
+
+for(int i = 0; i < n; i++)
+{
+    dict[numsN[i]] = 1;
+}
+
+foreach(int i in numsM)
+{
+    sw.Write(dict[i] + " ");
+}
+
+sw.Flush();
