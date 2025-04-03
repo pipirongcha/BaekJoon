365a7d39e931d9792ebee2dab79eeae1a1b4d1bc
Pipirongcha <pipirongcha0x0@gmail.com>
2025-04-03 10:32:46 +0900

    No.7785 Solved

---
+++ No.7785/No.7785/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Immutable;
+using System.Security.Cryptography;
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+Dictionary<string, string> dict =  new Dictionary<string, string>();
+int n = int.Parse(sr.ReadLine());
+int j = 0;
+int l = 0;
+
+for(int i = 0; i < n; i++)
+{
+    string[] input = sr.ReadLine().Split();
+    dict[input[0]] = input[1];
+}
+
+foreach (string key in dict.Keys)
+{
+    if (dict[key] == "enter")
+    {
+        l++;
+    }
+}
+
+string[] list = new string[l];
+
+foreach (string key in dict.Keys)
+{
+    if (dict[key] == "enter")
+    {
+        list[j] = key;
+        j++;
+    }
+}
+
+Array.Sort(list, (a, b) => b.CompareTo(a));
+
+for (int i = 0; i < list.Length; i++)
+{
+    sw.WriteLine(list[i]);
+}
+
+sw.Flush();
+
