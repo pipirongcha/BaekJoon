0ad6a49a55e6b92159aca19bd4b8ad2ddeaec7ea
Pipirongcha <pipirongcha0x0@gmail.com>
2025-04-06 01:13:22 +0900

    No.1764 Solved

---
+++ No.1764/No.1764/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StringBuilder sb = new StringBuilder();
+
+int[] nm = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+int n = nm[0];
+int m = nm[1];
+string[] names = new string[n + m];
+int res = 0;
+
+
+for (int i = 0; i < n + m; i++)
+{
+    names[i] = sr.ReadLine();
+}
+
+Array.Sort(names);
+
+for (int i = 0; i < n + m - 1; i++)
+{
+    if (names[i] == names[i + 1])
+    {
+        res++;
+    }
+}
+sb.AppendLine(res.ToString());
+
+for (int i = 0; i < n + m - 1; i++)
+{
+    if (names[i] == names[i + 1])
+    {
+        sb.AppendLine(names[i]);
+    }
+}
+Console.WriteLine(sb.ToString());
