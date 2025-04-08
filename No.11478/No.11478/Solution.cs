198e98414878a158259a4f9f72ae89ca87edf6ab
Pipirongcha <pipirongcha0x0@gmail.com>
2025-04-08 15:53:32 +0900

    No.11478 Solved

---
+++ No.11478/No.11478/Solution.cs
@@ -0,0 +1,14 @@
+﻿string target = Console.ReadLine();
+List<string> ssList = new List<string>();
+
+for (int i = 0; i < target.Length; i++)
+{
+    for (int j = 0; j < target.Length - i; j++)
+    {
+        ssList.Add(target.Substring(i, j + 1));
+    }
+}
+
+ssList = ssList.Distinct().ToList();
+
+Console.WriteLine(ssList.Count);
