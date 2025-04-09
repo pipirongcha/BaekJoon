7bbbb562d7167fd70f7398aaebe22732635945c2
Pipirongcha <pipirongcha0x0@gmail.com>
2025-04-09 16:19:31 +0900

    No.1934 Solved

---
+++ No.1934/No.1934/Solution.cs
@@ -0,0 +1,22 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+int t = int.Parse(sr.ReadLine());
+
+for (int i = 0; i < t; i++)
+{
+    int[] input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+    int a = Math.Max(input[0], input[1]);
+    int b = Math.Min(input[0], input[1]);
+    int r = 0;
+
+    while (b != 0)
+    {
+        r = a % b;
+        a = b;
+        b = r;
+    }
+    sw.WriteLine(input[0]*input[1]/ a);
+}
+
+sw.Flush();
