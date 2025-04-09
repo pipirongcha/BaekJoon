ed5dbe72bdc6c081047cdcc7102591fb7f5417a6
Pipirongcha <pipirongcha0x0@gmail.com>
2025-04-10 00:11:23 +0900

    No.13241 Solved

---
+++ No.13241/No.13241/Solution.cs
@@ -0,0 +1,10 @@
+﻿long[] input = Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+long a = Math.Max(input[0], input[1]);
+long b = Math.Min(input[0], input[1]);
+while(b!=0)
+{
+    long r = a % b;
+    a = b;
+    b = r;
+}
+Console.WriteLine(input[0] * input[1] / a);
