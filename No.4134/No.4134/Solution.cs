052b4dccecdab60ab25831827b4c72674b71e99b
Pipirongcha <pipirongcha0x0@gmail.com>
2025-04-13 16:08:54 +0900

    No.4134 Solved

---
+++ No.4134/No.4134/Solution.cs
@@ -0,0 +1,37 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+double n = double.Parse(sr.ReadLine());
+
+for (double i = 0; i < n; i++)
+{
+    double target = double.Parse(sr.ReadLine());
+    double j = 2;
+    if (target < 2)
+    {
+        sw.WriteLine(2);
+    }
+    else
+    {
+        while (true)
+        {
+            double tmp = target % j;
+            if (tmp == 0 && j <= Math.Sqrt(target))
+            {
+                target++;
+                j = 2;
+            }
+            else if (j >= Math.Sqrt(target))
+            {
+                break;
+            }
+            else
+            {
+                j++;
+            }
+        }
+        sw.WriteLine(target);
+    }
+}
+
+sw.Flush();
