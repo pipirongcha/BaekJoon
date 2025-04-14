d93b20b656b1e4e1c44bb181d669e2851e820f8d
Pipirongcha <pipirongcha0x0@gmail.com>
2025-04-14 10:11:07 +0900

    No.1929 Solved

---
+++ No.1929/No.1929/Solution.cs
@@ -0,0 +1,34 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+int i = 2;
+int[] input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+int m = input[0];
+int n = input[1];
+
+if(m < 2)
+{
+    m = 2;
+}
+
+while(m <= n)
+{
+    int tmp = m % i;
+    if(i>Math.Sqrt(m))
+    {
+        sw.WriteLine(m);
+        m++;
+        i=2;
+    }
+    else if(tmp == 0 && i <= Math.Sqrt(m))
+    {
+        m++;
+        i = 2;
+    }
+    else
+    {
+        i++;
+    }
+}
+
+sw.Flush();
