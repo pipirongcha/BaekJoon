4312bea1e8db35cddfc0d1ed3fb289708f6bcd12
Pipirongcha <pipirongcha0x0@gmail.com>
2025-04-16 17:05:52 +0900

    No.17103 Solved

---
+++ No.17103/No.17103/Solution.cs
@@ -0,0 +1,43 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+Dictionary<int,bool> primeNums = new Dictionary<int, bool>();
+primeNums[2] = false;
+for (int i = 3; i <= 1000000; i++)
+{
+    int j = 2;
+    while (true)
+    {
+        int tmp = i % j;
+        if (tmp == 0 && j <= Math.Sqrt(i))
+        {
+            break;
+        }
+        else if(j>Math.Sqrt(i))
+        {
+            primeNums[i] = false;
+            break;
+        }
+        else
+        {
+            j++;
+        }
+    }
+}
+
+int n = int.Parse(sr.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int res = 0;
+    int target = int.Parse(sr.ReadLine());
+    for (int j = 2; j <= target/2; j++)
+    {
+        if(primeNums.ContainsKey(j)&&primeNums.ContainsKey(target-j))
+        {
+            res++;
+        }
+    }
+    sw.WriteLine(res);
+}
+sw.Flush();
