ee14d7239ac1bb5fdd6b109078bb2e87c5fb72ed
Pipirongcha <pipirongcha0x0@gmail.com>
2025-04-19 03:02:46 +0900

    No.10773 Solved

---
+++ No.10773/No.10773/Solution.cs
@@ -0,0 +1,21 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+int k = int.Parse(sr.ReadLine());
+Stack<int> stack = new Stack<int>();
+int res = 0;
+
+for (int i = 0; i < k; i++)
+{
+    int input = int.Parse(sr.ReadLine());
+    if (input == 0)
+    {
+        int tmp = stack.Pop();
+        res -= tmp;
+    }
+    else
+    {
+        stack.Push(input);
+        res += input;
+    }
+}
+
+Console.Write(res);
