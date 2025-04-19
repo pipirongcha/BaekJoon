68e9dc4489e0887f829399e5ea4d996c1cd0279b
Pipirongcha <pipirongcha0x0@gmail.com>
2025-04-20 04:34:41 +0900

    No.9012 Solved

---
+++ No.9012/No.9012/Solution.cs
@@ -0,0 +1,35 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+int t = int.Parse(sr.ReadLine());
+
+for(int i = 0; i < t; i++)
+{
+    string target = sr.ReadLine();
+    int check = 0;
+
+    for(int j = 0; j < target.Length; j++)
+    {
+        if (check < 0)
+        {
+            break;
+        }
+        if(target[j] == '(')
+        {
+            check++;
+        }
+        else
+        {
+            check--;
+        }
+    }
+    if(check == 0)
+    {
+        sw.WriteLine("YES");
+    }
+    else
+    {
+        sw.WriteLine("NO");
+    }
+}
+
+sw.Flush();
