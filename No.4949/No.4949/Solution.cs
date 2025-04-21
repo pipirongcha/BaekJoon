6ad49ad7c4fee23435ee394c87e7299828e65bfb
Pipirongcha <pipirongcha0x0@gmail.com>
2025-04-21 10:15:12 +0900

    No.4949 Solved

---
+++ No.4949/No.4949/Solution.cs
@@ -0,0 +1,55 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+string input = sr.ReadLine();
+Stack<char> stack = new Stack<char>();
+while(input != ".")
+{
+    bool isBalanced = true;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+
+        if (input[i] == '(')
+        {
+            stack.Push(input[i]);
+        }
+        else if (input[i] == '[')
+        {
+            stack.Push(input[i]);
+        }
+        else if (input[i] == ')')
+        {
+            if(stack.Count == 0 || stack.Pop() != '(')
+            {
+                isBalanced = false;
+                break;
+            }
+        }
+        else if (input[i] == ']')
+        {
+            if (stack.Count == 0 || stack.Pop() != '[')
+            {
+                isBalanced = false;
+                break;
+            }
+        }
+    }
+    if (stack.Count != 0)
+    {
+        isBalanced = false;
+    }
+
+    if (isBalanced)
+    {
+        sw.WriteLine("yes");
+    }
+    else
+    {
+        sw.WriteLine("no");
+    }
+    input = sr.ReadLine();
+    stack.Clear();
+}
+
+sw.Flush();
