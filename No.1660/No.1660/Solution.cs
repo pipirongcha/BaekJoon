8fd8fcd2da56d0e02de030bb7c1cfe72ee83d902
Pipirongcha <pipirongcha0x0@gmail.com>
2025-04-23 22:07:43 +0900

    No.1660 Solved

---
+++ No.1660/No.1660/Solution.cs
@@ -0,0 +1,29 @@
+﻿int n = int.Parse(Console.ReadLine());
+List<int> list = new List<int>();
+int a = 1;
+int b = 1;
+
+while (b <= n)
+{
+    list.Add(b);
+    a++;
+    b += a * (a + 1) / 2;
+}
+
+int[] dp = new int[n+1];
+Array.Fill(dp, int.MaxValue);
+dp[0] = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    foreach (int j in list)
+    {
+        if (j > i)
+        {
+            break;
+        }
+        dp[i] = Math.Min(dp[i], dp[i - j] + 1);
+    }
+}
+
+Console.WriteLine(dp[n]);
