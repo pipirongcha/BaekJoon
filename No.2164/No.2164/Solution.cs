0a4981f3bd0d032f89097c4e8c4b00648ee532ae
Pipirongcha <pipirongcha0x0@gmail.com>
2025-04-24 07:06:26 +0900

    No.2164 Solved

---
+++ No.2164/No.2164/Solution.cs
@@ -0,0 +1,14 @@
+﻿int n = int.Parse(Console.ReadLine());
+Queue<int> queue = new Queue<int>();
+for (int i = 0; i < n; i++)
+{
+    queue.Enqueue(i + 1);
+}
+
+while (queue.Count != 1)
+{
+    queue.Dequeue();
+    queue.Enqueue(queue.Dequeue());
+}
+
+Console.WriteLine(queue.Dequeue());
