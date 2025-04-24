338e6d28a85cbea970b50e97e0a8d78392dbf1ce
Pipirongcha <pipirongcha0x0@gmail.com>
2025-04-24 09:47:35 +0900

    No.11866 Solved

---
+++ No.11866/No.11866/Solution.cs
@@ -0,0 +1,23 @@
+﻿StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+int[] nk = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int n = nk[0];
+int k = nk[1];
+sw.Write("<");
+Queue<int> queue = new Queue<int>();
+
+for (int i = 1; i <= n; i++)
+{
+    queue.Enqueue(i);
+}
+
+while (queue.Count != 1)
+{
+    for (int i = 1; i < k; i++)
+    {
+        queue.Enqueue(queue.Dequeue());
+    }
+    sw.Write($"{queue.Dequeue()}, ");
+}
+sw.Write($"{queue.Dequeue()}>");
+
+sw.Flush();
