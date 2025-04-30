41d09fdc28a5f61aec285cd30d183016ed9a2eef
Pipirongcha <pipirongcha0x0@gmail.com>
2025-04-30 16:56:09 +0900

    No.11050 Solved

---
+++ No.11050/No.11050/Solution.cs
@@ -0,0 +1,22 @@
+﻿int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int n = input[0];
+int k = input[1];
+int m = n - k;
+int a = 1;
+int b = 1;
+int c = 1;
+
+for (int i = 1; i <= n; i++)
+{
+    a *= i;
+    if (i <= k)
+    {
+        b *= i;
+    }
+    if (i <= m)
+    {
+        c *= i;
+    }
+}
+
+Console.Write(a / (b * c));
