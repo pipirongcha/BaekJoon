cda8756c236fa3ebc1005b164190d4e6923075d3
Pipirongcha <pipirongcha0x0@gmail.com>
2025-05-02 06:33:03 +0900

    No.1037 Solved

---
+++ No.1037/No.1037/Solution.cs
@@ -0,0 +1,19 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] factors = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int min = 2147483647;
+int max = 0;
+
+for (int i = 0; i < n; i++)
+{
+    if (factors[i] < min)
+    {
+        min = factors[i];
+    }
+    if (factors[i] > max)
+    {
+        max = factors[i];
+    }
+}
+
+Console.Write(min * max);
