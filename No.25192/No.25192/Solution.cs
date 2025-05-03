00ce31349fa320139ac0a1f70435859ab2a69815
Pipirongcha <pipirongcha0x0@gmail.com>
2025-05-03 16:40:15 +0900

    No.25192 Solved

---
+++ No.25192/No.25192/Solution.cs
@@ -0,0 +1,24 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+int n = int.Parse(sr.ReadLine());
+Dictionary<string,bool> dict = new Dictionary<string,bool>();
+int res = 0;
+
+for(int i = 0; i < n; i++)
+{
+    string input = sr.ReadLine();
+    if (input == "ENTER")
+    {
+        dict.Clear();
+    }
+    else
+    {
+        if (!dict.ContainsKey(input))
+        {
+            res++;
+            dict.Add(input, true);
+        }
+    }
+ 
+}
+
+Console.WriteLine(res);
