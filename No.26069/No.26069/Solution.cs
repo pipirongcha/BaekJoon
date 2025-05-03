7cf5c4b7dce6e6d2717bee160ffb127844fc538c
Pipirongcha <pipirongcha0x0@gmail.com>
2025-05-04 03:12:45 +0900

    No.26069 Solved

---
+++ No.26069/No.26069/Solution.cs
@@ -0,0 +1,31 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+HashSet<string> set = new HashSet<string>();
+int n = int.Parse(sr.ReadLine());
+int res = 0;
+for (int i = 0; i < n; i++)
+{
+    string[] input = sr.ReadLine().Split();
+    if (set.Contains(input[0]) || set.Contains(input[1]))
+    {
+
+        if (!set.Contains(input[0]))
+        {
+            res++;
+            set.Add(input[0]);
+
+        }
+        else if(!set.Contains(input[1]))
+        {
+            res++;
+            set.Add(input[1]);
+        }
+
+    }
+    else if (input[0] == "ChongChong" || input[1] == "ChongChong")
+    {
+        res+=2;
+        set.Add(input[0]);
+        set.Add(input[1]);
+    }
+}
+Console.Write(res);
