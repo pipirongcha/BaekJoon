f7d0e21950f9e0c7df384f860aa4c267d52516dd
Pipirongcha <pipirongcha0x0@gmail.com>
2025-05-07 15:05:24 +0900

    No.27433 Solved

---
+++ No.27433/No.27433/Solution.cs
@@ -0,0 +1,13 @@
+﻿long n = long.Parse(Console.ReadLine());
+
+long Factorial(long x)
+{
+    if (x <= 1)
+    {
+        return 1;
+    }
+
+    return x * Factorial(x-1);
+}
+
+Console.WriteLine(Factorial(n));
