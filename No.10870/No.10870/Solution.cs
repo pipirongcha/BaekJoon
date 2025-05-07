454590a0ce182029dc4febb749251e716472e985
Pipirongcha <pipirongcha0x0@gmail.com>
2025-05-07 15:07:00 +0900

    No.10870 Solved

---
+++ No.10870/No.10870/Solution.cs
@@ -0,0 +1,13 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int Fibonacci(int x)
+{
+    if (x <= 1)
+    {
+        return x;
+    }
+
+    return Fibonacci(x - 1) + Fibonacci(x - 2);
+}
+
+Console.WriteLine(Fibonacci(n));
