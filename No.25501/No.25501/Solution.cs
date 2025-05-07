cf325a995b23e772f841df8a765979ddce2551f8
Pipirongcha <pipirongcha0x0@gmail.com>
2025-05-07 19:54:55 +0900

    No.25501 Solved

---
+++ No.25501/No.25501/Solution.cs
@@ -0,0 +1,26 @@
+﻿StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+StreamReader sr = new StreamReader(Console.OpenStandardInput());
+int n = int.Parse(sr.ReadLine());
+int m = 0;
+for(int i = 0; i < n; i++)
+{
+    string s = sr.ReadLine();
+    m = 0;
+    Recursion(s, 0, s.Length - 1);
+    sw.WriteLine($"{Recursion(s, 0, s.Length - 1)} {m/2}");
+}
+int Recursion(string s, int l, int r)
+{
+    m++;
+    if (l >= r)
+    {
+        return 1;
+    }
+    else if (s[l] != s[r])
+    {
+        return 0;
+    }
+    else return Recursion(s, l + 1, r - 1);
+}
+
+sw.Flush();
