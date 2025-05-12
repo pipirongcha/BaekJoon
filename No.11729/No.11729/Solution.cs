2bad80b133c6ad89f0f0127eb738ab92450c29ce
Pipirongcha <pipirongcha0x0@gmail.com>
2025-05-12 17:37:56 +0900

    No.11729 Solved

---
+++ No.11729/No.11729/Solution.cs
@@ -0,0 +1,19 @@
+﻿StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+int n = int.Parse(Console.ReadLine());
+sw.WriteLine(Math.Pow(2,n)-1);
+
+Hanoi(n, 1, 2, 3);
+
+void Hanoi(int n, int dest, int transit ,int arr)
+{
+    if (n == 1)
+    {
+        sw.WriteLine($"{dest} {arr}");
+        return;
+    }
+    Hanoi(n-1, dest, arr, transit);
+    sw.WriteLine($"{dest} {arr}");
+    Hanoi(n-1, transit, dest, arr);
+}
+
+sw.Flush();
