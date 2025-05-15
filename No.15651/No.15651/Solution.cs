9726059c30ca4198c59e548bfe4eb95689473f0b
Pipirongcha <pipirongcha0x0@gmail.com>
2025-05-15 14:38:32 +0900

    No.15651 Solved

---
+++ No.15651/No.15651/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+StringBuilder sb = new StringBuilder();
+
+int[] nm = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int n = nm[0];
+int m = nm[1];
+int[] arr = new int[m];
+
+Combination(0);
+sw.WriteLine(sb.ToString());
+
+void Combination(int depth)
+{
+    if (m == depth)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            sb.Append(arr[i]+" ");
+        }
+        sb.Append("\n");
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[depth] = i + 1;
+        Combination(depth+1);
+    }
+}
+
+sw.Flush();
