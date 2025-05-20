468bef9c3df061b09a36a61d835dca95c00b45aa
Pipirongcha <pipirongcha0x0@gmail.com>
2025-05-20 18:10:33 +0900

    No.17478 Solved

---
+++ No.17478/No.17478/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine());
+StringBuilder sb = new StringBuilder();
+sb.AppendLine("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
+
+ChatBot(n, 0);
+Console.WriteLine(sb.ToString());
+
+void ChatBot(int n, int depth)
+{
+    string underBar = new string('_', depth*4);
+
+    if(n == 0)
+    {
+        sb.AppendLine(underBar + "\"재귀함수가 뭔가요?\"");
+        sb.AppendLine(underBar + "\"재귀함수는 자기 자신을 호출하는 함수라네\"");
+        sb.AppendLine(underBar + "라고 답변하였지.");
+        return;
+    }
+
+    sb.AppendLine(underBar + "\"재귀함수가 뭔가요?\"");
+    sb.AppendLine(underBar + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
+    sb.AppendLine(underBar + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
+    sb.AppendLine(underBar + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+
+    ChatBot(n-1, depth+1);
+    sb.AppendLine(underBar + "라고 답변하였지.");
+}
