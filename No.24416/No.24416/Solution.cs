2095a34271aa7aa627078001e63b50180d2c8a85
Pipirongcha <pipirongcha0x0@gmail.com>
2025-05-24 20:06:44 +0900

    No.24416 Solved

---
+++ No.24416/No.24416/Solution.cs
@@ -0,0 +1,22 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] f = new int[n];
+int dp = 0;
+long reculsive = 0;
+f[0] = 1;
+f[1] = 1;
+for (int i = 2; i < n; i++)
+{
+    dp++;
+    f[i] = f[i - 1] + f[i - 2];
+}
+Fib(n);
+int Fib(int n)
+{
+    if (n == 1 || n == 2)
+    {
+        reculsive++;
+        return 1;
+    }
+    return (Fib(n - 1) + Fib(n - 2));
+}
+Console.WriteLine($"{reculsive} {dp}");
