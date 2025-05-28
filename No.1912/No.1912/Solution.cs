a511d5c08c52493b49a45ee778a1516c4064cda4
Pipirongcha <pipirongcha0x0@gmail.com>
2025-05-28 12:59:47 +0900

    No.1912 Solved

---
+++ No.1912/No.1912/Solution.cs
@@ -0,0 +1,16 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+int n  = int.Parse(sr.ReadLine());
+int[] input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+
+int res = input[0];
+int current = input[0];
+
+
+for (int i = 1; i < n; i++)
+{
+    current = Math.Max(input[i], current + input[i]);
+    res = Math.Max(res, current); 
+}
+
+Console.WriteLine(res);
