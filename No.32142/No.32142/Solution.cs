571b87562ad6bf4a6b73651927cd42fd336b0dad
Pipirongcha <pipirongcha0x0@gmail.com>
2025-06-04 14:53:05 +0900

    No.32142 Solved

---
+++ No.32142/No.32142/Solution.cs
@@ -0,0 +1,23 @@
+﻿//StreamReader sr = new StreamReader(Console.OpenStandardInput());
+//int n = int.Parse(sr.ReadLine());
+//int[][] coord = new int[n][];
+
+//for(int i = 0; i < n; i++)
+//{
+//    int[] input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+//    coord[i][0] = input[0];
+//    coord[i][1] = input[1];
+//}
+
+//for(int i = 0;i < n-1;i++)
+//{
+//    int distance = Math.Sqrt(Math.Pow(coord[i][0] - coord[i + 1][0], 2) + Math.Pow(coord[i][1] - coord[i + 1][0], 2));
+//}
+
+Console.Write(-1);
+
+
+/*
+In all case, polygons could not be created....
+Answer was -1...
+ */
