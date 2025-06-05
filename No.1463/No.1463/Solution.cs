9488a34cf0f7ff25181d26b04a61d89bd0256213
Pipirongcha <pipirongcha0x0@gmail.com>
2025-06-05 15:16:55 +0900

    No.1463 Solved

---
+++ No.1463/No.1463/Solution.cs
@@ -0,0 +1,19 @@
+﻿int x = int.Parse(Console.ReadLine());
+int[] dp = new int[x+1];
+
+dp[1] = 0;
+
+for (int i = 2; i <= x; i++)
+{
+    dp[i] = dp[i - 1] + 1;
+    if (i % 3 == 0)
+    {
+        dp[i] = Math.Min(dp[i/3]+1, dp[i]);
+    }
+    if (i % 2 == 0)
+    {
+        dp[i] = Math.Min(dp[i/2]+1, dp[i]);
+    }
+}
+
+Console.WriteLine(dp[x]);
