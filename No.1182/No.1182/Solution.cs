9759593ef784277628a9e9b7fcb3f99afe1942ec
Pipirongcha <pipirongcha0x0@gmail.com>
2025-06-08 10:27:28 +0900

    No.1182 Solved

---
+++ No.1182/No.1182/Solution.cs
@@ -0,0 +1,33 @@
+﻿int[] ns = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int n = ns[0];
+int s = ns[1];
+int count = 0;
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+
+BackTracking(0, 0);
+
+void BackTracking(int depth, int sum)
+{
+    if (depth == n)
+    {
+        if (sum == s)
+        {
+            count++;
+        }
+        return;
+    }
+
+    BackTracking(depth + 1, sum + arr[depth]);
+    BackTracking(depth + 1, sum);
+
+}
+
+if (s == 0)
+{
+    Console.WriteLine(count - 1);
+}
+else
+{
+    Console.WriteLine(count);
+}
