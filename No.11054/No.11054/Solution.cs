5ae7e1f1315bfbc179e70bcfb1c236174785f478
Pipirongcha <pipirongcha0x0@gmail.com>
2025-06-11 16:25:25 +0900

    No.11054 Solved

---
+++ No.11054/No.11054/Solution.cs
@@ -0,0 +1,36 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int[] increase = new int[n];
+int[] decrease = new int[n];
+int res = 0;
+
+for (int i = 0; i < n; i++)
+{
+    increase[i] = 1;
+
+    for (int j = 0; j < i; j++)
+    {
+        if (arr[i] > arr[j])
+        {
+            increase[i] = Math.Max(increase[i], increase[j] + 1);
+        }
+    }
+}
+
+for (int i = n - 1; i >= 0; i--)
+{
+    decrease[i] = 0;
+    for (int j = n - 1; j > i; j--)
+    {
+        if (arr[i] > arr[j])
+        {
+            decrease[i] = Math.Max(decrease[i], decrease[j] + 1);
+        }
+    }
+}
+
+for (int i = 0; i < n; i++)
+{
+    res = Math.Max(res, increase[i] + decrease[i]);
+}
+Console.WriteLine(res);
