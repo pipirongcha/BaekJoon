0e123e4f08ab08b52911d1d2cded857f400783de
Pipirongcha <pipirongcha0x0@gmail.com>
2025-06-14 11:12:38 +0900

    No.9657 Solved

---
+++ No.9657/No.9657/Solution.cs
@@ -0,0 +1,15 @@
+﻿int n = int.Parse(Console.ReadLine());
+bool[] dp = new bool[1001];
+
+dp[1] = true;
+dp[2] = false;
+dp[3] = true;
+dp[4] = true;
+dp[5] = true;
+
+for(int i = 6; i <= n; i++)
+{
+    dp[i] = !(dp[i - 1] && dp[i - 3] && dp[i - 4]);
+}
+
+Console.Write(dp[n] ? "SK" : "CY");
