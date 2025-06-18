feefe98e4eadd24345b861398614941f5baa347c
Pipirongcha <pipirongcha0x0@gmail.com>
2025-06-18 17:31:50 +0900

    No.26041 Solved

---
+++ No.26041/No.26041/Solution.cs
@@ -0,0 +1,28 @@
+﻿string[] input = Console.ReadLine().Split();
+int count = 0;
+bool check = false;
+string head = Console.ReadLine();
+
+for(int i = 0; i < input.Length; i++)
+{
+
+    if (head.Length < input[i].Length)
+    {
+        check = true;
+        for (int j = 0; j < head.Length; j++)
+        {
+            if (input[i][j] != head[j])
+            {
+                check = false;
+                break;
+            }
+        }
+        if (check)
+        {
+            count++;
+            check = false;
+        }
+    }
+}
+
+Console.Write(count);
