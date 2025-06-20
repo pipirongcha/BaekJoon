5fad80c66316d6d2983baba5c8b934495749d524
Pipirongcha <pipirongcha0x0@gmail.com>
2025-06-20 10:17:31 +0900

    No.2559 Solved

---
+++ No.2559/No.2559/Solution.cs
@@ -0,0 +1,21 @@
+﻿int[] nk = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int n = nk[0];
+int k = nk[1];
+int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int max = 0;
+
+
+for(int i = 0; i < k; i++)
+{
+    max += input[i];
+}
+
+int tmp = max;
+
+for (int i = k; i < n; i++)
+{
+    tmp = tmp + input[i] - input[i - k];
+    max = Math.Max(max, tmp);
+}
+
+Console.WriteLine(max);
