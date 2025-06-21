21050ea179e578dd72de438d7c46ca38c0f985b3
Pipirongcha <pipirongcha0x0@gmail.com>
2025-06-21 14:05:02 +0900

    No.1920 Solved

---
+++ No.1920/No.1920/Solution.cs
@@ -0,0 +1,19 @@
+﻿StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+HashSet<int> set = new HashSet<int>();
+int n = int.Parse(sr.ReadLine());
+int[] a = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+int m = int.Parse(sr.ReadLine());
+int[] b = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+
+for (int i = 0; i < n; i++)
+{
+    set.Add(a[i]);
+}
+
+for(int i = 0; i < m; i++)
+{
+    sw.WriteLine(set.Contains(b[i]) ? 1 : 0);
+}
+
+sw.Flush();
