e769ffdfab950c5a2f1e8e6c96bc4c42921cda02
Pipirongcha <pipirongcha0x0@gmail.com>
2025-06-24 18:03:41 +0900

    No.1205 Solved

---
+++ No.1205/No.1205/Solution.cs
@@ -0,0 +1,31 @@
+﻿int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int n = input[0];
+int score = input[1];
+int p = input[2];
+
+if (n != 0)
+{
+    int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    List<int> ranking = arr.ToList();
+    ranking.Add(score);
+    ranking.Sort((a,b) => b.CompareTo(a));
+
+    int res = ranking.IndexOf(score) + 1;
+
+    if(res > p || n + 1 > p && ranking[p] == score)
+    {
+        Console.WriteLine(-1);
+    }
+    else
+    {
+        Console.WriteLine(res);
+    }
+
+}
+else
+{
+    Console.WriteLine(1);
+}
+
+
+
