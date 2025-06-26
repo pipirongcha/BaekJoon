c435db2bee0f010a354e632b63df388cc9892e5c
Pipirongcha <pipirongcha0x0@gmail.com>
2025-06-26 18:05:56 +0900

    No.10986 Solved

---
+++ No.10986/No.10986/Solution.cs
@@ -0,0 +1,25 @@
+﻿int[] nm = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int n = nm[0];
+int m = nm[1];
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+long [] modCount = new long[m];
+long sum = 0;
+long res = 0;
+
+modCount[0] = 1;
+
+
+for(int i = 0; i < n; i++)
+{
+    sum += arr[i];
+    int mod = (int)(sum % m);
+    modCount[mod]++;
+}
+
+for(int i = 0; i < m; i++)
+{
+    res += modCount[i] * (modCount[i] - 1) / 2;
+}
+
+Console.WriteLine(res);
