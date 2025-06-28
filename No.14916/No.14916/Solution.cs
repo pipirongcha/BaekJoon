9226f48709c642b383dfc2ee3f5b4d94293b47e6
Pipirongcha <pipirongcha0x0@gmail.com>
2025-06-28 19:21:37 +0900

    No.14916 Solved

---
+++ No.14916/No.14916/Solution.cs
@@ -0,0 +1,21 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] dp = new int[100001];
+int m = 100001;
+Array.Fill(dp, m);
+
+dp[1] = m;
+dp[2] = 1;
+dp[3] = m;
+dp[4] = 2;
+dp[5] = 1;
+
+for(int i = 6; i <= n; i++)
+{
+    if (dp[i - 5] != m || dp[i - 2] != m)
+    {
+        dp[i] = Math.Min(dp[i-5] + 1 , dp[i-2] + 1);
+    }
+
+}
+
+Console.Write(dp[n] == m ? -1 : dp[n]);
